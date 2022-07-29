b59ea810ef9528e1e0e786766f1927af710486f4
ITZABHXD <107246484+ITZABHXD@users.noreply.github.com>
2022-07-29 10:40:12 +0530

    Add files via upload

---
+++ sql_helpers/__init__.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+DB_URI = os.environ.get("DATABASE_URL", None)
+
+if DB_URI:
+    DB_URI = DB_URI.replace('postgres://', 'postgresql://')
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
